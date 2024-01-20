57169f8078145042a2e115b9da08c1032ba499e3
Syed Amer <syedaidid584@gmail.com>
2024-01-20 18:31:14 +0800

    change database from sqlite to mysql

--- UPTMHousingHub/settings.py
+++ UPTMHousingHub/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = "UPTMHousingHub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'UPTMHH',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
