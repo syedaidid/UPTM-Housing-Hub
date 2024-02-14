ea86c2029497e344f09d378a11c8aa23c52a9c94
Syed Amer <syedaidid584@gmail.com>
2024-02-15 01:17:25 +0800

    created list, detail, update & delete view

--- UPTMHousingHub/settings.py
+++ UPTMHousingHub/settings.py
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'syedaidid584@gmail.com'
-EMAIL_HOST_PASSWORD = 'nrek zfxa ykyj hyww'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
